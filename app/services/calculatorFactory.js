9e2bf438c5467ffea8f61e3ae770dcaf683c0cf2
rbowness <rbowness20@gmail.com>
2018-09-20 08:48:31 -0400

    Fixed Dice Match for Saves

--- app/services/calculatorFactory.js
+++ app/services/calculatorFactory.js
@@ -19,7 +19,11 @@
         ) {
             var hits = attacks * ((7 - toHit) / 6);
 
+            //Figure out the toWound value
             switch (true) {
+                case strength >= (toughness * 2):
+                    var toWound = 2;
+                    break;
                 case strength > toughness:
                     var toWound = 3;
                     break;
@@ -29,12 +33,15 @@
                 case strength < toughness:
                     var toWound = 5;
                     break;
+                case strength <= toughness / 2  :
+                    var toWound = 6;
+                    break;    
             }
 
             var wounds = hits * ((7 - toWound) / 6)
 
-            //TODO: Fix the math here.
-            var modifiedSave = armourSave + armourPen;
+            //Calculate the actual save
+            var modifiedSave = parseInt(armourSave) + parseInt(armourPen);
             if(modifiedSave <= 1)
             {
                 modifiedSave = 2;
@@ -44,7 +51,15 @@
                 modifiedSave = 0;
             }
 
-            var unsavedWounds = wounds * (1 - ((7 - modifiedSave) / 6));
+            //Bypass for when no save is allowed.
+            if(modifiedSave == 0)
+            {
+                var unsavedWounds = wounds; // The save is 0 or the AP of the attack is too high to allow an armour save.
+            }
+            else
+            {
+                var unsavedWounds = wounds * (1 - ((7 - modifiedSave) / 6));
+            }
 
             var damageAverage = unsavedWounds * damage;
 
